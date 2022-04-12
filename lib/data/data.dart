90b0471f9ca7c46c5837e5107d628744a86a3777
afonso.burginski <afonso.burginski@topsapp.com.br~>
2022-04-12 09:29:27 -0400

    Update images

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,9 +1,9 @@
 import 'package:flutter_facebook_responsive_ui/models/models.dart';
 
 final User currentUser = User(
-  name: 'Marcus Ng',
+  name: 'Afonso Burginski',
   imageUrl:
-      'https://images.unsplash.com/photo-1578133671540-edad0b3d689e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80',
+      'https://images.unsplash.com/photo-1649765154772-d6e26435d36f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=648&q=80',
 );
 
 final List<User> onlineUsers = [
@@ -167,7 +167,7 @@ final List<Story> stories = [
 final List<Post> posts = [
   Post(
     user: currentUser,
-    caption: 'Check out these cool puppers',
+    caption: 'Meus filhotes já estão crescidos!',
     timeAgo: '58m',
     imageUrl: 'https://images.unsplash.com/photo-1525253086316-d0c936c814f8',
     likes: 1202,
@@ -177,7 +177,7 @@ final List<Post> posts = [
   Post(
     user: onlineUsers[5],
     caption:
-        'Please enjoy this placeholder text: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        'Mesmo que eu seja um programador, na vida, não acredito em lógica condicional. Mesmo conseguindo escrever o algoritmo, jamais conseguiremos executá-lo e explorar os dois argumentos, pois o tempo não retrocede.',
     timeAgo: '3hr',
     imageUrl: null,
     likes: 683,
@@ -186,17 +186,17 @@ final List<Post> posts = [
   ),
   Post(
     user: onlineUsers[4],
-    caption: 'This is a very good boi.',
+    caption: 'Meu melhor amigo ❤️.',
     timeAgo: '8hr',
     imageUrl:
-        'https://images.unsplash.com/photo-1575535468632-345892291673?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
+        'https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
     likes: 894,
     comments: 201,
     shares: 27,
   ),
   Post(
     user: onlineUsers[3],
-    caption: 'Adventure 🏔',
+    caption: 'Momentos Incríveis 🏔',
     timeAgo: '15hr',
     imageUrl:
         'https://images.unsplash.com/photo-1573331519317-30b24326bb9a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
@@ -207,7 +207,7 @@ final List<Post> posts = [
   Post(
     user: onlineUsers[0],
     caption:
-        'More placeholder text for the soul: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        'Faça como um programador. Quando tudo está errado e confuso, apague tudo e recomece do zero.',
     timeAgo: '1d',
     imageUrl: null,
     likes: 482,
@@ -216,7 +216,7 @@ final List<Post> posts = [
   ),
   Post(
     user: onlineUsers[9],
-    caption: 'A classic.',
+    caption: 'A clássica Londres 💈',
     timeAgo: '1d',
     imageUrl:
         'https://images.unsplash.com/reserve/OlxPGKgRUaX0E1hg3b3X_Dumbo.JPG?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
